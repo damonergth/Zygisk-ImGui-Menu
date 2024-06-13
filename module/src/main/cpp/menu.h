8e78be9c68b655475d657013f7b0d235daf70d12
Bangdap <134949593+Dapz123@users.noreply.github.com>
2024-06-14 03:36:24 +0700

    Remove my menu
    the menu will be moved

--- module/src/main/cpp/menu.h
+++ module/src/main/cpp/menu.h
@@ -1,37 +1,6 @@
 #ifndef IMGUI_MENU_H
 #define IMGUI_MENU_H
-
-using namespace ImGui;
-
-int i1 = 0;
-float f1 = 0.123f;
-
-void DrawMenu() {
-    static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f); 
-    {
-        Begin(OBFUSCATE("EZCHEATS"));
-        ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_FittingPolicyResizeDown;
-        if (BeginTabBar("Menu", tab_bar_flags)) {
-            if (BeginTabItem(OBFUSCATE("PLAYER"))) {
-                Checkbox(OBFUSCATE("One Hit"), &attackScale);
-                Checkbox(OBFUSCATE("Dumb Enemt"), &bot);
-                Checkbox(OBFUSCATE("No CD"), &mana);
-                Checkbox(OBFUSCATE("Hp Update"), &updHp);
-                Checkbox(OBFUSCATE("Frame Updater"), &updFrame);
-                Checkbox(OBFUSCATE("Attach"), &attach);
-                EndTabItem();
-            }
-            SliderInt("slider int", &dmg, 0, 100);
-            SliderFloat("slider float", &f1, 0.0f, 1.0f, "ratio = %.3f");
-            EndTabBar();
-            
-        }
-        TextUnformatted(OBFUSCATE ("BY DAPZ"));
-        Patches();
-        End();
-    }
-}
-
+ using namespace ImGui;
 void SetupImgui() {
     IMGUI_CHECKVERSION();
     CreateContext();
