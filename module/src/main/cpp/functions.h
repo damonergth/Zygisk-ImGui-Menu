0a7038ef5676f7bcf1802284ead88c804fec12b9
Bangdap <134949593+Dapz123@users.noreply.github.com>
2024-06-14 10:59:53 +0700

    update module/src/main/cpp/functions.h

--- module/src/main/cpp/functions.h
+++ module/src/main/cpp/functions.h
@@ -74,4 +74,39 @@ void DrawMenu() {
     }
 }
 
+void SetupImgui() {
+    IMGUI_CHECKVERSION();
+    CreateContext();
+    ImGuiIO &io = GetIO();
+    io.DisplaySize = ImVec2((float) glWidth, (float) glHeight);
+    ImGui_ImplOpenGL3_Init("#version 100");
+    StyleColorsDark();
+    ImFontConfig font_cfg;
+    font_cfg.SizePixels = 22.0f;
+    io.Fonts->AddFontDefault(&font_cfg);
+    GetStyle().ScaleAllSizes(7.0f);
+}
+
+EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
+EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    eglQuerySurface(dpy, surface, EGL_WIDTH, &glWidth);
+    eglQuerySurface(dpy, surface, EGL_HEIGHT, &glHeight);
+
+    if (!setupimg) {
+        SetupImgui();
+        setupimg = true;
+    }
+
+    ImGuiIO &io = GetIO();
+    ImGui_ImplOpenGL3_NewFrame();
+    NewFrame();
+
+    DrawMenu();
+
+    EndFrame();
+    Render();
+    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return old_eglSwapBuffers(dpy, surface);
+}
 #endif // FUNCTION_H
